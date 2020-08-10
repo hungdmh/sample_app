af330751d4cb715f78f2672f66ce24230a0343cc
Manh Hung <do.manh.hungb@sun-asterisk.com>
2020-08-10 09:35:45 +0700

    Chapter 14

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,14 +3,15 @@ class Micropost < ApplicationRecord
 
   has_one_attached :image
 
-  scope :recent_posts, ->{order created_at: :desc}
+  scope :date_desc_posts, ->{order created_at: :desc}
+  scope :users_feed, ->(user_ids){where user_id: user_ids}
 
   validates :user_id, presence: true
   validates :content, presence: true,
-    length: {maximum: Settings.validations.micropost.max_length}
+                      length: {maximum: Settings.validations.micropost.max_length}
   validates :image,
-    content_type: {in: %w(image/jpeg image/gif image/png),
-                   message: I18n.t("microposts.errors.image_format")},
-    size: {less_than: Settings.size.max_file_size.megabytes,
-           message: I18n.t("microposts.errors.size_too_big")}
+            content_type: {in: Settings.validations.image_content_type,
+                           message: I18n.t("microposts.errors.image_format")},
+            size: {less_than: Settings.size.max_file_size.megabytes,
+                   message: I18n.t("microposts.errors.size_too_big")}
 end
